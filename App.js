097e565318ec70a27c399fb97954a0e00d0f3815
“soniharshil54” <sharshil43@yahoo.com>
2024-04-16 16:57:19 +0530

    intial commit, goal add, list working

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [ goalInput, setGoalInput] = useState('')
+  const [ goalList, setGoalList ] = useState([]);
+
+  function handleGoalInputChange(input) {
+    console.log('input', input)
+    setGoalInput(input)
+  }
+
+  function handleGoalAdd() {
+    setGoalList((previousGoalList) => [ ...previousGoalList, goalInput ])
+    setGoalInput("")
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.title}>
+        <TextInput style={{width: 235}} value={goalInput} placeholder='enter goal' onChangeText={handleGoalInputChange}></TextInput>
+        <Button title='add goal' onPress={handleGoalAdd}/>
+      </View>
+      <View style={styles.goalList}>
+        {
+          goalList.map((goal, i) => (
+            <Text key={i}>{goal}</Text>
+          ))
+        }
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +37,29 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    marginVertical: 50,
+    borderWidth: 1,
+    borderColor: 'blue',
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    padding: 10
+  },
+
+  title: {
+    borderWidth: 1,
+    borderColor: 'red',
+    padding: 10,
+    flexDirection: 'row',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
   },
+
+  goalList: {
+    borderWidth: 1,
+    borderColor: 'green',
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+
 });
