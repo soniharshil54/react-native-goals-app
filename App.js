bb58d8bd5560c3c6a8ff7a8c1a7e785e763ba90e
“soniharshil54” <sharshil43@yahoo.com>
2024-04-19 15:05:22 +0530

    crud working

--- App.js
+++ App.js
@@ -4,29 +4,74 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
 
-  const [ goalInput, setGoalInput] = useState('')
+  const [ goalFormData, setGoalFormData] = useState({
+    goalName: ''
+  })
   const [ goalList, setGoalList ] = useState([]);
 
-  function handleGoalInputChange(input) {
-    console.log('input', input)
-    setGoalInput(input)
+  function handleGoalInputChange(inputName, inputValue) {
+    setGoalFormData({
+      ...goalFormData,
+      [inputName]: inputValue
+    })
   }
 
   function handleGoalAdd() {
-    setGoalList((previousGoalList) => [ ...previousGoalList, goalInput ])
-    setGoalInput("")
+    setGoalList((previousGoalList) => [ ...previousGoalList, {
+      ...goalFormData,
+      id: new Date().getTime().toString()
+    } ])
+    setGoalFormData({
+      goalName: ''
+    })
+  }
+
+  function handleGoalDelete(id) {
+    setGoalList((previousGoalList) => previousGoalList.filter((goal) => goal.id !== id))
+  }
+
+  function handleGoalView(id) {
+    setGoalFormData(goalList.find((goal) => goal.id === id));
+  }
+
+  function handleGoalUpdate(id) {
+    setGoalList((goalList) => {
+      return goalList.map((goal) => {
+        if (goal.id === id) {
+          return goalFormData
+        }
+        return goal
+      })
+    })
+    setGoalFormData({
+      goalName: ''
+    })
   }
 
   return (
     <View style={styles.container}>
       <View style={styles.title}>
-        <TextInput style={{width: 235}} value={goalInput} placeholder='enter goal' onChangeText={handleGoalInputChange}></TextInput>
-        <Button title='add goal' onPress={handleGoalAdd}/>
+        <TextInput style={{width: 235}} value={goalFormData.goalName} placeholder='enter goal' onChangeText={(text) => {
+          handleGoalInputChange('goalName', text)
+        }}></TextInput>
+        <Button title={goalFormData && goalFormData.id ? 'Update' : 'Add' } onPress={() => {
+          if (goalFormData && goalFormData.id) {
+            handleGoalUpdate(goalFormData.id)
+          } else {
+            handleGoalAdd()
+          }
+        }}/>
       </View>
       <View style={styles.goalList}>
         {
           goalList.map((goal, i) => (
-            <Text key={i}>{goal}</Text>
+            <View key={goal.id} style={styles.goal}>
+              <Text>{goal.goalName}</Text>
+              <View style={{flexDirection: 'row'}}>
+              <Button title='Delete' onPress={() => handleGoalDelete(goal.id)}/>
+              <Button title='Edit' onPress={() => handleGoalView(goal.id)}/>
+              </View>
+            </View>
           ))
         }
       </View>
@@ -43,6 +88,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     justifyContent: 'center',
     padding: 10
+  },
+
+  goal: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
 
   title: {
